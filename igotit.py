5c872665bd6be1870eac45b444efa5d499febad7
483759 <483759@naver.com>
2019-07-30 23:58:30 +0900

    챗봇 구현 완료

--- igotit.py
+++ igotit.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
 from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import requests
 import urllib
-import bs4
 
 ERROR_MESSAGE = '네트워크 접속에 문제가 발생하였습니다. 잠시 후 다시 시도해주세요.'
 
@@ -9,33 +10,36 @@ ERROR_MESSAGE = '네트워크 접속에 문제가 발생하였습니다. 잠시 
 app = Flask(__name__)
 
 
-# 피자 주문 스킬
-@app.route('/order', methods=['POST'])
-def order():
+@app.route('/weather', methods=['POST'])
+def weather():
 
-    # 메시지 받기
     req = request.get_json()
 
-    #pizza_type = req["action"]["detailParams"]["피자종류"]["value"]
-    #address = req["action"]["detailParams"]["sys_text"]["value"]
     location = req["action"]["detailParams"]["sys_location"]["value"]
 
-    enc_location = urllib.parse.quote(location + ' + 날씨')
-    url = 'https://search.naver.com/search.naver?sm=top_hty&fbm=1&ie=utf8&query='+ enc_location
+    enc_loc = urllib.parse.quote(location + '+ 날씨')
+    el = str(enc_loc)
+    url = 'https://search.naver.com/search.naver'
+    url = url + '?sm=top_hty&fbm=1&ie=utf8&query='
+    url = url + el
 
     req = Request(url)
     page = urlopen(req)
     html = page.read()
-    soup = bs4.BeautifulSoup(html,)
-    rain_pct=soup.find('li',class_='on now merge1').find('dd',class_='weather_item _dotWrapper').text
+    soup = BeautifulSoup(html, 'html.parser')
+    r1 = soup.find('li', class_='on now merge1')
+    r2 = r1.find('dd', class_='weather_item _dotWrapper')
+    r3 = r2.find('span').text
+
+    rain_pct = int(r3)
 
     if len(location) <= 0:
         answer = ERROR_MESSAGE
+    elif rain_pct < 30:
+        answer = location + "의 강수 확률은 " + r3 + "%입니다 맑은 하루 되세요^_^"
     else:
-        answer = location + "의 강수확률은" + rain_pct + "입니다 우산 챙겨가세요!"
-        #answer = pizza_type + "를 '" + address + "'(으)로 배달하겠습니다.주문 감사합니다~"
+        answer = location + "의 강수 확률은 " + r3 + "%입니다 우산 챙겨가세요!!"
 
-    # 메시지 설정
     res = {
         "version": "2.0",
         "template": {
@@ -55,4 +59,4 @@ def order():
 # 메인 함수
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', port=5000, threaded=True)
